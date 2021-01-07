b170b5dbe376be3e2f6d3054320b95947c327e1f
messiashub <claudineimessias@hotmail.com>
2021-01-07 14:52:01 -0300

    definido direcoes e impedir que cobra saia da tela

--- script.js
+++ script.js
@@ -14,24 +14,39 @@ function criarBG() {
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarCobra(){
-    for(i=0; i < snake.length; i++){
+function criarCobra() {
+    for (i = 0; i < snake.length; i++) {
         context.fillStyle = 'green';
-        context.fillRect(snake[i].x, snake[i].y,box, box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-function iniciarJogo(){
+document.addEventListener('keydown', update); // keydown = tecla
+
+function update(mover) {
+    if (mover.keyCode == 37 && direction != "rigth") direction = "left";
+    if (mover.keyCode == 38 && direction != "down") direction = "up";
+    if (mover.keyCode == 39 && direction != "left") direction = "rigth";
+    if (mover.keyCode == 40 && direction != "up") direction = "down";
+
+}
+
+function iniciarJogo() {
+    if (snake[0].x > 15 * box && direction == 'rigth') snake[0].x = 0;
+    if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
+    if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
     criarBG();
     criarCobra();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(direction == "rigth") snakeX += box;
-    if(direction == "left") snakeX -= box;
-    if(direction == "up") snakeY -= box;
-    if(direction == "down") snakeY += box;
+    if (direction == "rigth") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
 
     snake.pop();
 
@@ -39,9 +54,9 @@ function iniciarJogo(){
         x: snakeX,
         y: snakeY
     }
-    
+
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo,100);
+let jogo = setInterval(iniciarJogo, 100);
 
